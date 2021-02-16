e622d2b645c013255267c6542af68e6c5d8db848
James Gavin Holt <jamesgavinholt1@gmail.com>
2021-02-16 14:47:56 -0500

    - try again

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ function HomePage() {
     <div>
       <Head>
         <base href="/"/>
-        <meta charset="utf-8"/>
+        <meta charSet="utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
         
